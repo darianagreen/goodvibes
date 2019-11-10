f4d619d99ab88641a13e3ec3da8df607e9d00b65
Dariana Green <darianapro@gmail.com>
2019-11-10 04:49:52 +0100

    GoodVibes ver.3 Songs list added and styled

--- album.php
+++ album.php
@@ -4,6 +4,8 @@ include("includes/handlers/header.php");
 include("includes/config.php");
 include("includes/classes/Artist.php");
 include("includes/classes/Album.php");
+include("includes/classes/Song.php");
+
 
 
 if(isset($_SESSION['userLoggedIn'])) {
@@ -51,11 +53,60 @@ if(isset($_SESSION['userLoggedIn'])) {
 
                     <div class="leftSection">
                          <img src="<?php echo $album->getArtworkPath(); ?>">
+                         <h2><?php echo $album->getTitle(); ?></h2>
+                            <span>By <?php echo $artist->getName(); ?></span>
+                           
                     </div>
 
                     <div class="rightSection">
-                            <h2><?php echo $album->getTitle(); ?></h2>
-                            <span>By <?php echo $artist->getName(); ?></span>
+                            <h2 id="songsList"> Songs </h2>
+                            <div class="tracklistContainer">
+                            <ul class="tracklist">
+		
+		<?php
+		$songIdArray = $album->getSongIds();
+
+		$i = 1;
+		foreach($songIdArray as $songId) {
+
+			$albumSong = new Song($con, $songId);
+			$albumArtist = $albumSong->getArtist();
+
+			echo "<li class='tracklistRow'>
+					<div class='trackCount'>
+						<img class='play' src='assets/images/icons/play_white2.png'>
+						<span class='trackNumber'>$i</span>
+                    </div>
+                    
+                    <div class='trackInfo'>
+                    <span class='trackName'>" . $albumSong->getTitle() . "</span>
+                    <span class='artistName'>" . $albumArtist->getName() . "</span>
+                </div>
+
+                <div class='trackOptions'>
+                    <img class='optionsButton' src='assets/images/icons/more.png'>
+                </div>
+
+                <div class='trackDuration'>
+                    <span class='duration'>" . $albumSong->getDuration() . "</span>
+                </div>
+
+				</li>";
+
+			$i = $i + 1;
+
+
+
+		}
+
+		?>
+
+
+
+	</ul>
+
+                            </div>
+                           
                     </div>
 
                 </div>
